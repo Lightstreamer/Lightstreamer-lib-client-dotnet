0f182966e23ce847f66d6248652e80a36fb7b31c
giuseppe.corti <giuseppe.corti@lightstreamer.com>
2020-02-06 12:33:32 +0100

    Remove temp working folder

--- MyClientLibrary/MyClientLibrary/com/lightstreamer/client/transport/SessionRequestListener.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace com.lightstreamer.client.transport
-{
-    /// <summary>
-    /// Listener handling a data connection (in streaming or polling).
-    /// </summary>
-    public interface SessionRequestListener : RequestListener
-    {
-    }
-}
