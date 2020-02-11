1f71432d9520b5f0bc4f86c3dbc6845f4b8f53c0
giuseppe.corti <giuseppe.corti@lightstreamer.com>
2020-02-11 18:11:20 +0100

    Update CreateSessionRequest.cs

--- Lightstreamer_DotNet_Client_Unified/com/lightstreamer/client/requests/CreateSessionRequest.cs
+++ Lightstreamer_DotNet_Client_Unified/com/lightstreamer/client/requests/CreateSessionRequest.cs
@@ -44,7 +44,7 @@ namespace com.lightstreamer.client.requests
             this.addParameter("LS_polling_millis", requestedPollingInterval);
             this.addParameter("LS_idle_millis", requestedIdleTimeout);
 
-            this.addParameter("LS_cid", "jqWtj1twChtfDxikwp1ltvcB4BJ4Ji kOj2CKt");
+            this.addParameter("LS_cid", "jqWtj1twChtfDxikwp1ltvc64CJ4Ji kOj2Cr");
 
             if (options.InternalMaxBandwidth == 0)
             {
